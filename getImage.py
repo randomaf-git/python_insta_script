d60853d945d17cb0c1d3ce70a59300825d3f97bd
randomaf-git <53532266+randomaf-git@users.noreply.github.com>
2019-08-01 21:18:58 +0700

    finished the script
    now the script are able to take (almost) all the photo from a public instagram profile. Only taking single photos, no videos and or multi-photo post. how many photos taken depends on how long you set the getImageDuration.

--- getImage.py
+++ getImage.py
@@ -1,33 +1,45 @@
 import requests
 import requests_html
+import time
 import sys
 import re
 import webbrowser
 import urllib
 from selenium import webdriver
-
-def getPhoto(urlString):
-	r = requests.get(urlString)
-	pageSource = r.text.split("\n")
-	for imageSource in pageSource:
-		imageUrl = getImageMeta(imageSource)
-		if(imageUrl):
-			imageName = getImageName(imageUrl)
-			urllib.request.urlretrieve(imageUrl,imageName+'.jpg')
-			break
-
-def getImageMeta(inputURL):
-	m = re.search('^    <meta property="og:image" content="(.*)" />',inputURL)
-	if(m):
-		return m.group(1)
-
+from selenium.webdriver.chrome.options import Options
+#Function to scroll the browser to the bottom of the page
+def scrollPage(browser):
+	pauseTime = 1
+	browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+	time.sleep(pauseTime)
+#Get all the loaded image Href list on a profile page
 def getImageHrefList(inputURL):
 	m = re.findall('<a href="/p/(.*?)/">',inputURL)
 	if(m):
 		return m
 	else:
 		print("Nope. Not found.")
-
+#Function to determine how long to scroll and getting photo post url
+def getImageDuration(duration,browser):
+	timeout = time.time() + duration
+	innerHTML = browser.execute_script("return document.body.innerHTML")
+	postUrlList = getImageHrefList(innerHTML)
+	while True:
+		scrollPage(browser)
+		innerHTML = browser.execute_script("return document.body.innerHTML")
+		morePostUrlList = getImageHrefList(innerHTML)
+		for postUrl in morePostUrlList:
+			if postUrl not in postUrlList:
+				postUrlList.append(postUrl)
+		if time.time()>timeout:
+			break
+	return postUrlList
+#Get the image meta content property for a single post
+def getImageMeta(inputURL):
+	m = re.search('^    <meta property="og:image" content="(.*)" />',inputURL)
+	if(m):
+		return m.group(1)
+#Get the image name for a single post
 def getImageName(imageMeta):
 	m = re.search('//(.*).jpg',imageMeta)
 	if(m):
@@ -41,15 +53,30 @@ def getImageName(imageMeta):
 			i = i-1
 		return "local-filename.jpg"
 	return "local-filename.jpg"
+#Save the photo from a single post
+def getPhoto(urlString):
+	r = requests.get(urlString)
+	pageSource = r.text.split("\n")
+	for imageSource in pageSource:
+		imageUrl = getImageMeta(imageSource)
+		if(imageUrl):
+			imageName = getImageName(imageUrl)
+			urllib.request.urlretrieve(imageUrl,imageName+'.jpg')
+			break
 
 def getAllImageFromPage(profileUrl):
-	browser = webdriver.Chrome()
+	options = Options()
+	options.headless = True
+	browser = webdriver.Chrome(chrome_options=options)
 	browser.get(profileUrl)
-	innerHTML = browser.execute_script("return document.body.innerHTML")
+	singlePostUrlList = getImageDuration(15,browser)
 	browser.quit()
-	singlePostUrlList = getImageHrefList(innerHTML)
+	print("getAllImageFromPage: Getting", len(singlePostUrlList), "amount of post")
 	for singleUrl in singlePostUrlList:
 		urlString = 'https://www.instagram.com/p/' + singleUrl + '/'
 		getPhoto(urlString)
+
+
+
 
 getAllImageFromPage(sys.argv[1])
